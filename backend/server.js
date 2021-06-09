36573d68fdc18168506007cec4dcdb292469ba26
Maksym Harasymchuk <harasymchuk.m.v@gmail.com>
2021-06-10 02:21:18 +0300

    the last try

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,7 @@ app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
 
 const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/frontend/build')));
@@ -42,6 +43,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(
   PORT,
   console.log(
-    `Server running in ${process.env.NODE_ENV} mode on ${PORT}`.underline.cyan
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
   )
 );
